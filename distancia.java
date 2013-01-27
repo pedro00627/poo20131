789c4ad26bbd0f5d33b2b721b3263751967453af
Pedro Rodriguez <pedro00627@gmail.com>
2013-01-27 16:48:39 -0500

    comentarios añadidos

--- distancia.java
+++ distancia.java
@@ -1,14 +1,14 @@
 import java.lang.Math;
-
+//hecho por Pedro Andrés Rodríguez León 625234
 class distancia 
 	{ 
 	public static void main(String[] args) { 
-		double xi = 0.0; 
-		double a = 9.8; 
-		double t = 10.0; 
-		double vi = 2.0; 
-		double x = 0.5*a*(Math.pow(t, 2))+vi*t+xi;
-		System.out.println ("la distacia recorrida es: "+x);
+		double xi = 0.0; //inicialización de la variable distancia inicial
+		double a = 9.8; //aceleración
+		double t = 10.0; //variable tiempo
+		double vi = 2.0; //velicidad inicial
+		double x = 0.5*a*(Math.pow(t, 2))+vi*t+xi; //fórmula de distacia con respecto al tiempo
+		System.out.println ("la distacia recorrida es: "+x); //muestra el resultado de la función
 		
 
 	} 
