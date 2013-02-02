48d2310a2425ed97cdd58aa64d92995edc57ecf2
Pedro Rodriguez <pedro00627@gmail.com>
2013-02-01 23:11:48 -0500

    DoMath2

---
+++ DoMath2.java
@@ -0,0 +1,12 @@
+class DoMath2 
+{
+	public static void main(String[] arguments) 
+	{
+		double puntaje = 1.0 + 2.0 * 3.0;
+		System.out.println(puntaje);
+		double copia = puntaje;
+		copia = copia / 2.0;
+		System.out.println(copia);
+		System.out.println(puntaje);
+	}
+}
