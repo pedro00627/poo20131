1ab25a7d155e9f888eb55028a66e8d51691b6157
Pedro Rodriguez <pedro00627@gmail.com>
2013-02-01 22:59:52 -0500

    ingresar valores de las variables

--- distancia.java
+++ distancia.java
@@ -1,15 +1,34 @@
-import java.lang.Math;
 //hecho por Pedro Andrés Rodríguez León 625234
+
+import java.io.*;
+import java.lang.Math;
+
 class distancia 
 	{ 
 	public static void main(String[] args) { 
-		double xi = 0.0; //inicialización de la variable distancia inicial
-		double a = 9.8; //aceleración
-		double t = 10.0; //variable tiempo
-		double vi = 2.0; //velicidad inicial
-		double x = 0.5*a*(Math.pow(t, 2))+vi*t+xi; //fórmula de distacia con respecto al tiempo
-		System.out.println ("la distacia recorrida es: "+x); //muestra el resultado de la función
+	
+		//inicialización de variables y asignacion de valores
+		System.out.println("ingrese el valor de la distancia inicial");
+		String s=System.console().readLine();
+		double xi = Double.parseDouble(s); //inicialización de la variable distancia inicial
+		System.out.println("ingrese el valor de la aceleracion");
+		s=System.console().readLine();
+		double a = Double.parseDouble(s); //aceleración
+		System.out.println("ingrese el valor del tiempo");
+		s=System.console().readLine();
+		double t = Double.parseDouble(s);//variable tiempo
+		System.out.println("ingrese el valor de la velocidad inicial");
+		s=System.console().readLine();
+		double vi = Double.parseDouble(s); //velocidad inicial
 		
+		// operaciones 
+				
+		double x = 0.5*a*(Math.pow(t, 2))+vi*t+xi; //fórmula de distacia con respecto al tiempo
+		
+		//impresion del resultado
+		
+		System.out.println ("la distacia recorrida es: "+x); //muestra el resultado de la función
+
 
 	} 
 }
