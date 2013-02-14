2fad77f926d6158ad1b1aeab1fc106ba81a0c207
Pedro Rodriguez <pedro00627@gmail.com>
2013-02-13 21:03:49 -0500

    Update competencia.java
    corregido el error que no tenia en cuenta otros valores cuando empezaba el numero menor

--- competencia.java
+++ competencia.java
@@ -3,21 +3,22 @@
 class competencia
 {
 	public static void main(String[] args){
-	
+
 		//Ingresar cantidad de competidores
 		System.out.println("ingrese la cantidad de competidores: ");
 		int n=Integer.parseInt(System.console().readLine());
-		
+
 		//valores donde se almacenaran lo  mejores tiempos
 		int uno=0;
 		int dos=0;
 		double aux=999999999;
-		
-		
+		double aux2=999999999;
+
+
 		//inicialización de vectores
 		String[] nombre = new String[n];
 		double[] tiempo = new double[n];
-		
+
 		//introducir nombres
 		for(int i=0;i<n;i++){
 			System.out.println("ingrese el nombre del competidor "+(i+1)+(": "));
@@ -29,18 +30,21 @@ class competencia
 			System.out.println("ingrese el tiempo del competidor: "+(i+1)+", "+nombre[i]);
 			tiempo[i]=Double.parseDouble(System.console().readLine());
 		}
-			
-	
+
+
 		//busqueda de los dos mejores tiempos}
 
 		for(int i=0;i<n;i++){
 			if(tiempo[i]<aux){
+			aux2=aux;
 			aux=tiempo[i];
 			dos=uno;
 			uno=i;
-			}
+			} else if (tiempo[i]<=aux2){
+				dos=i;
+				}
 		}
-		
+
 		//resultados
 		System.out.println("El mejor competidor fue: "+nombre[uno]+" con un tiempo de: "+tiempo[uno]);
 		System.out.println("El segundo mejor competidor fue: "+nombre[dos]+" con un tiempo de: "+tiempo[dos]);
